ce303f9554475a708a693585bb123415e7172e33
Olga <1234@example.com>
2022-08-07 13:47:02 +0300

    Добавила решение

--- Program.cs
+++ Program.cs
@@ -8,4 +8,24 @@ a = -9; b = -3 -> max = -3
 Console.WriteLine("Введите 2 числа");
 int a, b;
 Console.WriteLine("Введите 1 число: ");
+a = Convert.ToInt32(Console.WriteLine());
+Console.WriteLine("Введите 2 число: ");
+b = Convert.ToInt32(Console.WriteLine());
+
+bool Chto0 = a > b;
+bool Chto1 = a == b;
+bool Chto2 = a < b;
+
+if (Chto0)
+{
+Console.WriteLine($"Число {a} больше чем {b}");
+}
+else if (Chto1)
+{
+Console.WriteLine($"Числа {a} и {b} равны");
+}
+else
+{
+Console.WriteLine($"Число {b} больше чем {a}");
+}
 
