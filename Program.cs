616087f8d67743fb26c2278553ce1fdcc954fff6
Olga <1234@example.com>
2022-08-07 20:21:19 +0300

    Добавила решение

--- Program.cs
+++ Program.cs
@@ -4,28 +4,28 @@ a = 5; b = 7 -> max = 7
 a = 2; b = 10 -> max = 10
 a = -9; b = -3 -> max = -3
 */
+    {
+        Console.WriteLine("Введите 2 числа");
+        int a, b;
+        Console.WriteLine("Введите 1ое число: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите 2ое число: ");
+        b = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите 2 числа");
-int a, b;
-Console.WriteLine("Введите 1 число: ");
-a = Convert.ToInt32(Console.WriteLine());
-Console.WriteLine("Введите 2 число: ");
-b = Convert.ToInt32(Console.WriteLine());
+        bool var0 = a > b;
+        bool var1 = a == b;
+        bool var2 = a < b;
 
-bool Chto0 = a > b;
-bool Chto1 = a == b;
-bool Chto2 = a < b;
-
-if (Chto0)
-{
-Console.WriteLine($"Число {a} больше чем {b}");
-}
-else if (Chto1)
-{
-Console.WriteLine($"Числа {a} и {b} равны");
-}
-else
-{
-Console.WriteLine($"Число {b} больше чем {a}");
-}
-
+        if (var0)
+        {
+        Console.WriteLine($"Число {a} больше чем {b}");
+        }
+        else if (var1)
+        {
+        Console.WriteLine($"Числа {a} и {b} равны");
+        }
+        else
+        {
+        Console.WriteLine($"Число {b} больше чем {a}");
+        }
+    }
